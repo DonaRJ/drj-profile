973fdee0e6e22622a6e0e0cfa935f8259b88528a
DonaRJ <donarani1993@gmail.com>
2020-07-31 22:42:02 +0530

    => Home page created => Added translate modules

--- src/app/components/components-routing.module.ts
+++ src/app/components/components-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home.component';
+import { ComponentsComponent } from './components.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{ path: '', component: ComponentsComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class HomeRoutingModule {}
+export class ComponentsRoutingModule {}
